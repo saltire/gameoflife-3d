2346d2004fe1d06d7cc00e20903190fb8c012763
saltire sable <saltiresable@gmail.com>
2017-08-15 22:52:01 -0400

    Add starfield, camera timeline, and PP bloom to spaceship scene

---
+++ Assets/StarfieldScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarfieldScript : MonoBehaviour {
+	public GameObject star;
+	public int count = 100;
+
+	void Start() {
+		Bounds bounds = GetComponent<Collider>().bounds;
+		Vector3 max = bounds.max;
+		Vector3 min = bounds.min;
+
+		for (int i = 0; i < count; i++) {
+			GameObject newStar = Instantiate(star, bounds.min + new Vector3(bounds.extents.x * Random.value, bounds.extents.y * Random.value, bounds.extents.z * Random.value) * 2, Quaternion.identity);
+			newStar.transform.parent = transform;
+		}
+	}
+}
