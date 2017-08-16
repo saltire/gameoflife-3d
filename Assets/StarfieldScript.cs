2d9ae83ddf1825dbd7ebf039526fd9d3c66454fc
saltire sable <saltiresable@gmail.com>
2017-08-15 23:18:24 -0400

    Stars move to give illusion of speed

--- Assets/StarfieldScript.cs
+++ Assets/StarfieldScript.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StarfieldScript : MonoBehaviour {
-	public GameObject star;
+	public GameObject Star;
 	public int count = 100;
+	public float speed = 5;
+
+	Bounds bounds;
+	List<Transform> stars;
 
 	void Start() {
-		Bounds bounds = GetComponent<Collider>().bounds;
-		Vector3 max = bounds.max;
-		Vector3 min = bounds.min;
+		bounds = GetComponent<Collider>().bounds;
+
+		stars = new List<Transform>();
 
 		for (int i = 0; i < count; i++) {
-			GameObject newStar = Instantiate(star, bounds.min + new Vector3(bounds.extents.x * Random.value, bounds.extents.y * Random.value, bounds.extents.z * Random.value) * 2, Quaternion.identity);
-			newStar.transform.parent = transform;
+			GameObject star = Instantiate(Star, bounds.min + new Vector3(bounds.extents.x * Random.value, bounds.extents.y * Random.value, bounds.extents.z * Random.value) * 2, Quaternion.identity);
+			star.transform.parent = transform;
+			stars.Add(star.transform);
+		}
+	}
+
+	void Update() {
+		foreach (Transform star in stars) {
+			star.position -= new Vector3(0, 0, speed);
+			if (star.position.z < bounds.min.z) {
+				star.position += new Vector3(0, 0, bounds.extents.z * 2);
+			}
 		}
 	}
 }
